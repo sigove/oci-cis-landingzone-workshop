c64dcbe33c84311f1d3644e5e1c813739558c7d2
Sebastián Aguado B <sebastian.aguado@oracle.com>
2023-07-26 12:21:04 +0200

    setting up git ewnvironment

--- configure-oci-shell.sh
+++ configure-oci-shell.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 curl https://github.com/cli/cli/releases/download/v2.32.1/gh_2.32.1_linux_386.tar.gz --output gh.tar.gz
-tar -xf gh.tar.gz
-export PATH="~/gh/bin/:$PATH"
+tar -xf gh_2.32.1_linux_386.tar.gz
+export PATH="$HOME/gh/bin/:$PATH"
 
-gh auth login
-gh repo clone 
+# Authenticate to github and get also the user scope
+gh auth login -s user
+
+gh repo clone oci-cis-landingzone-workshop-nose
+cd oci-cis-landingzone-workshop-nose
+
+git config user.name "$(gh api user -q .login)"
+git config user.email "$(gh api user/public_emails -q first.email)"
