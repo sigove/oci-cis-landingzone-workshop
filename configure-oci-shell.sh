2f21e9f0b1abb36bedca073a3828717ea684a675
Sebastián Aguado B <sebastian.aguado@oracle.com>
2023-07-26 13:30:45 +0200

    addfing creation of stack

--- configure-oci-shell.sh
+++ configure-oci-shell.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-curl https://github.com/cli/cli/releases/download/v2.32.1/gh_2.32.1_linux_386.tar.gz --output gh.tar.gz
-tar -xf gh_2.32.1_linux_386.tar.gz
-export PATH="$HOME/gh/bin/:$PATH"
+wget https://github.com/cli/cli/releases/download/v2.32.1/gh_2.32.1_linux_386.tar.gz -O gh.tar.gz
+tar -xf gh.tar.gz
+export PATH="$HOME/gh_2.32.1_linux_386/bin/:$PATH"
 
 # Authenticate to github and get also the user scope
 gh auth login -s user
@@ -12,3 +12,7 @@ cd oci-cis-landingzone-workshop-nose
 
 git config user.name "$(gh api user -q .login)"
 git config user.email "$(gh api user/public_emails -q first.email)"
+
+git_config_ocid=$(oci resource-manager configuration-source-provider create-github-access-token-provider --compartment-id "$OCI_CMP_ID" --access-token "$(gh auth token)" --api-endpoint "https://github.com"  --display-name "gh-lz-01" --query data.id --raw-output) 
+
+oci resource-manager stack create-from-git-provider --compartment-id "$OCI_CMP_ID" --config-source-configuration-source-provider-id "$git_config_ocid" --config-source-branch-name main --config-source-repository-url "$(gh repo view --json url -q .url)" --config-source-working-directory config --display-name "stack-gh-oci-lz-01" --terraform-version "1.2.x"
