434ada406edcc85a6d567410e151525b73da1254
Sebastián Aguado B <sebastian.aguado@oracle.com>
2023-08-24 09:44:55 +0200

    Fixing parameters

--- configure-oci-shell.sh
+++ configure-oci-shell.sh
@@ -8,13 +8,13 @@ export PATH="$HOME/gh_2.32.1_linux_386/bin/:$PATH"
 # Authenticate to github and get also the user scope
 gh auth login -s user -h github.com -p https -w
 
-gh repo clone saguadob/oci-cis-landingzone-workshop-nose
+gh repo clone oci-cis-landingzone-workshop-nose
 cd oci-cis-landingzone-workshop-nose
 
 git config user.name "$(gh api user -q .login)"
 git config user.email "$(gh api user/public_emails -q first.email)"
-gh repo set-default saguadob/oci-cis-landingzone-workshop-nose
+gh repo set-default "$(gh api user -q .login)/oci-cis-landingzone-workshop-nose"
 
 git_config_ocid=$(oci resource-manager configuration-source-provider create-github-access-token-provider --compartment-id "$OCI_TENANCY" --access-token "$(gh auth token)" --api-endpoint "https://github.com"  --display-name "gh-lz-01" --query data.id --raw-output) 
 
-oci resource-manager stack create-from-git-provider --compartment-id "$OCI_TENANCY" --config-source-configuration-source-provider-id "$git_config_ocid" --config-source-branch-name prepare --config-source-repository-url "$(gh repo view --json url -q .url)" --config-source-working-directory lz --display-name "stack-gh-oci-lz-01" --terraform-version "1.2.x"
+oci resource-manager stack create-from-git-provider --compartment-id "$OCI_TENANCY" --config-source-configuration-source-provider-id "$git_config_ocid" --config-source-branch-name main --config-source-repository-url "$(gh repo view --json url -q .url)" --config-source-working-directory lz --display-name "stack-gh-oci-lz-01" --terraform-version "1.2.x" --variables "{\"tenancy_ocid\":\"$OCI_TENANCY\" , \"region\":\"$OCI_REGION\"}"
