5b3e623d7ca45d1ce5cfb21fdbdc02367d1c1af9
Sebastián Aguado B <sebastian.aguado@oracle.com>
2023-08-24 15:21:06 +0200

    Adding workflow file

--- configure-oci-shell.sh
+++ configure-oci-shell.sh
@@ -15,6 +15,4 @@ git config user.name "$(gh api user -q .login)"
 git config user.email "$(gh api user/public_emails -q first.email)"
 gh repo set-default "$(gh api user -q .login)/oci-cis-landingzone-workshop-nose"
 
-git_config_ocid=$(oci resource-manager configuration-source-provider create-github-access-token-provider --compartment-id "$OCI_TENANCY" --access-token "$(gh auth token)" --api-endpoint "https://github.com"  --display-name "gh-lz-01" --query data.id --raw-output) 
-
-oci resource-manager stack create-from-git-provider --compartment-id "$OCI_TENANCY" --config-source-configuration-source-provider-id "$git_config_ocid" --config-source-branch-name main --config-source-repository-url "$(gh repo view --json url -q .url)" --config-source-working-directory lz --display-name "stack-gh-oci-lz-01" --terraform-version "1.2.x" --variables "{\"tenancy_ocid\":\"$OCI_TENANCY\" , \"region\":\"$OCI_REGION\"}"
+ 
